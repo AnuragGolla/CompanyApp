a49e5eca3f54daca21e38a79cb5b6ca2a078383b
Anurag Golla <agolla07@gmail.com>
2017-07-05 16:00:23 -0600

    group creation and load to db finished, not including group view

--- checkGroupMember_v3.php
+++ checkGroupMember_v3.php
@@ -16,17 +16,101 @@ if (!$conn) {
 $enterGroupName = $_REQUEST['groupName'];
 $enterMemberEmail = $_REQUEST['addedMember'];
 
-echo "Group Name: " . $enterGroupName;
-echo "<br>Member Email: " . $enterMemberEmail;
+//echo "Group Name: " . $enterGroupName;
+//echo "<br>Member Email: " . $enterMemberEmail;
 
-$addMemberQuery = "INSERT INTO synkeioGroups (groupName, userEmail)
+
+$checkMember = "SELECT *
+FROM googleUsers
+WHERE email = '$enterMemberEmail';";
+
+
+$queryObject = mysqli_query($conn, $checkMember);
+
+$num_rows = mysqli_num_rows($queryObject);
+
+//echo "Rows: $num_rows";
+
+
+$addActivity = "INSERT INTO synkeioGroups (groupName, userEmail)
 VALUES ('$enterGroupName', '$enterMemberEmail');";
+
+
+if ($num_rows > 0) {
+  mysqli_query($conn, $addActivity);
+  include 'groupCreationForm_v3.html';
+} else {
+  echo "<br>Error: This email address is not a registered Synke.io user. Please try again.";
+}
+
+
+/*
+$queryNum = mysqli_fetch_assoc($queryObject);
+
+echo "<br>" . $queryNum[0];
+
+$queryInt = (int)$queryNum;
+
+echo "<br>" . $queryInt;
+*/
+
+
+
+
+
+
+
+
+
+/*
+$addMemberQuery = "INSERT INTO synkeioGroups (groupName, userEmail)
+VALUES ('$enterGroupName', '$enterMemberEmail');";*/
 /*WHERE EXISTS (SELECT *
 FROM googleUsers
 WHERE email = $enterMemberEmail);";*/
 
+/*
+$checkMember = "SELECT COUNT(*)
+FROM googleUsers
+WHERE email = '$enterMemberEmail';";
+
+echo "<br>" . $checkMember;
+
+$queryObject = mysqli_query($conn, $checkMember);
+
+//echo "<br>" . $queryObject;
+
+$queryNum = mysqli_fetch_assoc($queryObject);
+
+echo "<br>" . $queryNum[0];
+
+$queryInt = (int)$queryNum;
+
+echo "<br>" . $queryInt;
+*/
+
+/*
+if (mysqli_query($conn, $checkMember) > 0) {
+  echo "<br>yay";
+} else {
+  echo "<br>nooo";
+}
+*/
+
+//$testResult = mysqli_query($conn, $sql);
+
+/*
+if (mysqli_query($conn, $checkMember)) {
+    echo "<br>Query successful";
+} else {
+    echo "<br>Error.";
+}
+*/
+
+/*
 $result = mysqli_query($conn, $addMemberQuery);
 echo "<br>result= " . $result;
+*/
 
 /*
 $showGroup = "SELECT (email)
