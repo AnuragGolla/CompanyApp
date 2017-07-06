87f1c48f3e60a8e78ab115a45d17677670beabeb
Anurag Golla <agolla07@gmail.com>
2017-07-06 17:41:01 -0600

    working on DB load

--- loadEvent.php
+++ loadEvent.php
@@ -6,14 +6,17 @@ $dbname = "synkeio_database";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
+    echo "Connection success echo";
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+    echo "Connection failed echo";
 }
 
+
+/*
 //check if this is the best initialization
-$dbEventName = 0;
+$dbEventName = "";
 $dbGroupName = "";
 $dbStartDate = "";
 $dbEndDate = "";
@@ -56,20 +59,35 @@ if (isset($_POST['loca_tion']))
 {
   $dbLocation = $_POST["loca_tion"];
 }
+*/
 
-
-
+/*
   $dbEventName = $_POST["event_Name"];
   $dbGroupName = $_POST["group_Name"];
   $dbStartDate = $_POST["event_Start_Date"];
   $dbEndDate = $_POST["event_End_Date"];
   $dbStartTime = $_POST["event_Start_Time"];
   $dbEndTime = $_POST["event_End_Time"];
-  $dbLocation = $_POST["loca_tion"];
+  $dbLocation = $_POST["loca_Tion"];
+  $dbMeetingTime = $_POST["meeting_Length"];
+*/
+
+$dbEventName = "sampleEvent";
+$dbGroupName = "testy";
+$dbStartDate = "07/08/17";
+$dbEndDate = "07/15/17";
+$dbStartTime = "12:00:PM";
+$dbEndTime = "05:00:PM";
+$dbLocation = "Boulder";
+$dbMeetingTime = 120;
 
 
-$eventData = "INSERT INTO synkeioEvents (GroupName, EventName, StartDate, EndDate, StartTime, EndTime, Location)
-VALUES ('$dbGroupName', '$dbEventName', '$dbStartDate', '$dbEndDate', '$dbStartTime', '$dbEndTime', '$dbLocation');";
+  echo "<script>console.log($dbEventName)</script>";
+
+
+
+$eventData = "INSERT INTO synkeioEvents (GroupName, EventName, StartDate, EndDate, StartTime, EndTime, Location, meetingTime)
+VALUES ('$dbGroupName', '$dbEventName', '$dbStartDate', '$dbEndDate', '$dbStartTime', '$dbEndTime', '$dbLocation', '$dbMeetingTime');";
 
 $checker = "SELECT *
 FROM synkeioGroups
@@ -84,7 +102,7 @@ if ($numRows > 0) {
   echo "Success!";
 } else {
   include 'eventCreate.html';
-  echo "Error: The Group Name that you entered does not exist. Please try again."
+  echo "Error: The Group Name that you entered does not exist. Please try again.";
 }
 
 
