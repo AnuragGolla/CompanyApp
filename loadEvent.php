45c248601c28dadddb5cbcec7d048da04d560e1e
Anurag Golla <agolla07@gmail.com>
2017-07-06 22:31:22 -0600

    fixed bugs

--- loadEvent.php
+++ loadEvent.php
@@ -6,7 +6,7 @@ $dbname = "synkeio_database";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-    echo "Connection success echo";
+    echo "Connection success";
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -14,15 +14,16 @@ if (!$conn) {
 }
 
 
-/*
+
 //check if this is the best initialization
 $dbEventName = "";
 $dbGroupName = "";
-$dbStartDate = "";
-$dbEndDate = "";
-$dbStartTime = "";
-$dbEndTime = "";
+$dbStartDate = NULL;
+$dbEndDate = NULL;
+$dbStartTime = NULL;
+$dbEndTime = NULL;
 $dbLocation = "";
+$dbMeetingLength = 0;
 
 
 if (isset($_POST['event_Name']))
@@ -55,11 +56,16 @@ if (isset($_POST['event_End_Time']))
   $dbEndTime = $_POST["event_End_Time"];
 }
 
-if (isset($_POST['loca_tion']))
+if (isset($_POST['loca_Tion']))
 {
-  $dbLocation = $_POST["loca_tion"];
+  $dbLocation = $_POST["loca_Tion"];
 }
-*/
+
+if (isset($_POST['meeting_Length']))
+{
+  $dbMeetingLength = $_POST["meeting_Length"];
+}
+
 
 /*
   $dbEventName = $_POST["event_Name"];
@@ -72,6 +78,11 @@ if (isset($_POST['loca_tion']))
   $dbMeetingTime = $_POST["meeting_Length"];
 */
 
+
+echo "$dbEventName" . "<br>" . "$dbGroupName" . "<br>" . "$dbStartDate" . "<br>" . "$dbEndDate" . "<br>" . "$dbStartTime" . "<br>" . "$dbEndTime" . "<br>" . "$dbLocation" . "<br>" . "$dbMeetingLength";
+
+
+/*
 $dbEventName = "sampleEvent";
 $dbGroupName = "testy";
 $dbStartDate = "07/08/17";
@@ -80,31 +91,35 @@ $dbStartTime = "12:00:PM";
 $dbEndTime = "05:00:PM";
 $dbLocation = "Boulder";
 $dbMeetingTime = 120;
+*/
+
+  //echo "<script>console.log($dbEventName)</script>";
 
 
-  echo "<script>console.log($dbEventName)</script>";
-
-
-
+/*
 $eventData = "INSERT INTO synkeioEvents (GroupName, EventName, StartDate, EndDate, StartTime, EndTime, Location, meetingTime)
 VALUES ('$dbGroupName', '$dbEventName', '$dbStartDate', '$dbEndDate', '$dbStartTime', '$dbEndTime', '$dbLocation', '$dbMeetingTime');";
 
 $checker = "SELECT *
 FROM synkeioGroups
-WHERE groupName = $dbGroupName;";
+WHERE groupName = '$dbGroupName';";
 
 $query = mysqli_query($conn, $checker);
 
-$numRows = mysqli_num_rows($query);
+//var_dump($query);
+//echo "<br> Here: ";
 
-if ($numRows > 0) {
+$num_rows = mysqli_num_rows($query);
+//var_dump($num_rows);
+
+
+if ($num_rows > 0) {
   mysqli_query($conn, $eventData);
   echo "Success!";
 } else {
-  include 'eventCreate.html';
   echo "Error: The Group Name that you entered does not exist. Please try again.";
 }
 
-
+*/
 mysqli_close($conn);
 ?>
