e081df43f63b418cc7dbfee96b35756d957f787d
Anurag Golla <agolla07@gmail.com>
2017-07-13 09:05:54 -0600

    created back end for algorithm

--- algorithmDbLoad.php
+++ algorithmDbLoad.php
@@ -6,22 +6,46 @@ $dbname = "synkeio_database";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-    //echo "Connection success";
+    echo "Connection success<br>";
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
     //echo "Connection failed echo";
 }
 
-$groupsListQ = "SELECT groupName
+$thisName = "";
+if (isset($_POST['gName']))
+{
+  $thisName = $_POST["gName"];
+}
+$thisName = $_POST["gName"];
+
+
+
+$groupsListQ = "SELECT DISTINCT userEmail
 FROM synkeioGroups
-WHERE userEmail = $thisEmail;";
+WHERE groupName = '$thisName';";
 
+$request = mysqli_query($conn, $groupsListQ);
 
+$listedUserEvents = array();
 
+while ($row = mysqli_fetch_assoc($request)) {
+  //echo ($row["userEmail"]) . "<br>";
+  $thisEmail = $row["userEmail"];
 
+  $innerRequest = "SELECT googleEvents
+  FROM googleUsers
+  WHERE email = '$thisEmail';";
 
+  $eventString = mysqli_query($conn, $innerRequest);
+  $eventStringMod = mysqli_fetch_assoc($eventString);
 
+  $listedUserEvents[] = $eventStringMod["googleEvents"];
+
+}
+
+//can use $listedUserEvents string as the output of array of strings of all users in the entered group
 
 
 mysqli_close($conn);
